7305ffaf64ff7920d773ca4735e0edeb48574954
Malpp <cpt.ortan@gmail.com>
2017-03-24 12:21:16 -0400

    Init

---
+++ Pacman/Application.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+using System.Diagnostics;
+
+namespace Pacman
+{
+	class Application
+	{
+
+		#region Global vars
+
+		//The window of application
+		private RenderWindow window;
+
+		//Used to time the movement of the objects on screen
+		Clock clock = new Clock();
+		Time gameTime = new Time();
+
+		#endregion
+
+		/// <summary>
+		/// Constructor of the window
+		/// </summary>
+		/// <param name="windowHeight">Height of the window</param>
+		/// <param name="windowWidth">Width of the window</param>
+		/// <param name="title">Title of the window</param>
+		/// <param name="style">Style of the window</param>
+		public Application(uint windowHeight = 300, uint windowWidth = 300, string title = "SFML APP", Styles style = Styles.Close)
+		{
+
+			window = new RenderWindow(new VideoMode(windowWidth, windowHeight), title, style);
+
+			//Add the keypressed function to the window
+			window.KeyPressed += window_KeyPressed;
+
+			//Add the Closed function to the window
+			window.Closed += window_Closed;
+
+		}
+
+
+		/// <summary>
+		/// Main loop of the program
+		/// </summary>
+		public void Run()
+		{
+
+			window.SetVisible(true);
+
+			while (window.IsOpen)
+			{
+
+				//Call the Events
+				window.DispatchEvents();
+
+				//Update the game
+				Update();
+
+				//Draw the updated app
+				Draw();
+
+			}
+
+
+		}
+
+		#region Input functions
+
+		/// <summary>
+		/// Called whenever a key is pressed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e">The key event</param>
+		void window_KeyPressed(object sender, KeyEventArgs e)
+		{
+
+			//Debug output of keyPressed
+			//Console.WriteLine(e.Code);
+
+			switch (e.Code)
+			{
+
+				case Keyboard.Key.Escape:
+					window.Close();
+					break;
+
+				default:
+					break;
+
+			}
+		}
+
+		/// <summary>
+		/// Called when the window "X" is clicked
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void window_Closed(object sender, EventArgs e)
+		{
+
+			window.Close();
+
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Update code of the program
+		/// </summary>
+		private void Update()
+		{
+
+			gameTime = clock.Restart();
+
+		}
+
+		/// <summary>
+		/// Draw code of the program
+		/// </summary>
+		private void Draw()
+		{
+
+			window.Clear();
+
+			window.Display();
+
+		}
+
+	}
+}
